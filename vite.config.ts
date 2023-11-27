a0c9ef730e5b734ae778c7acd804410cc95b72e8
Mizan-Rifat <mizanrifat001@gmail.com>
2023-11-27 10:25:54 +0600

    configs updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    checker({
+      typescript: true
+    })
+  ],
+  preview: {
+    port: 5000
+  },
+  server: {
+    port: 3000
+  }
+});
